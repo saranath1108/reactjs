59634def3a1c33c8e69a9196b9b5042b433613d2
saranath <saranathgupta@gmail.com>
2023-10-28 13:47:59 +0400

    pushing first app

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,8 @@
 //const heading = React.createElement("h1", {}, "Hello World from React");
 
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 tag from first child"),
@@ -7,9 +10,9 @@ const heading = React.createElement("div", { id: "parent" }, [
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "This is h1 tag from second child"),
-    React.createElement("h2", {}, "This is h2 tag from second child"),
+    React.createElement("h2", {}, "This is h2 tag from 2nd child"),
   ]),
 ]);
 
-const root = ReactDOM.render(heading, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(heading);
